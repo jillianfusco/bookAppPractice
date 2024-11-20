7063d772b1dfe76cbfa73cfb229eaf96056d0856
Jillian Fusco <fuscojillian@gmail.com>
2024-11-20 12:05:31 -0500

    created homeController and index.html and form.html however form results in no static resource error

---
+++ BookAppPractice/src/main/java/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/form")
+    public String displayForm() {
+        return "form";
+    }
+}
