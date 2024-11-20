187d31739da42a9d4878c78c59e7f2bd312eeb79
Jillian Fusco <fuscojillian@gmail.com>
2024-11-20 12:59:12 -0500

    added bookController but /books not rendering

--- BookAppPractice/src/main/java/controllers/BookController.java
+++ BookAppPractice/src/main/java/controllers/BookController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+@RequestMapping("books")
+public class BookController {
 
-    @GetMapping("")
-    public String index() {
-        return "index";
+    @GetMapping
+    public String displayBooks() {
+        return "books";
     }
-
 }
