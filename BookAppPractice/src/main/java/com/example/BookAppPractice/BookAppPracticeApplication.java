16327c1dee753eef24baa4f8dc543d78ea61a38c
Jillian Fusco <fuscojillian@gmail.com>
2024-11-20 11:59:03 -0500

    initial commit round 2

---
+++ BookAppPractice/src/main/java/com/example/BookAppPractice/BookAppPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookAppPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAppPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAppPracticeApplication.class, args);
+	}
+
+}
